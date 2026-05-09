eb97f64723023e696d1b5a2c5c11e5bb2f069379
Kevindrade7 <kevindrade7@gmail.com>
2026-05-09 17:27:19 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 
+function closePromo(){
+ document.getElementById('promoPopup').style.display='none';
+}
+
+window.onload = function(){
+ setTimeout(()=>{
+   document.getElementById('promoPopup').style.display='flex';
+ },1500);
+}
